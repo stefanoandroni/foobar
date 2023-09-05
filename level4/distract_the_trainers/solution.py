5b65bfef55555fbc3bdf04068c248615e94a39af
Stefano Androni <65281018+stefanoandroni@users.noreply.github.com>
2023-09-05 22:09:48 +0200

    Update level4\distract_the_trainers\solution.py

--- level4/distract_the_trainers/solution.py
+++ level4/distract_the_trainers/solution.py
@@ -1,31 +1,68 @@
-def solution(banana_list):
-    E = get_possible_pairs_leading_to_infinite_loop(banana_list) # E: set of all possible pairs leading to an infinite loop
-    MCM = get_maximum_cardinality_matching(E) # MCM: maximum cardinality matching (maximum independent edge set) of G=(V,E)
-    return len(banana_list) -  2 * MCM
+def solution(*args):
+    banana_list = args[0] if len(args) == 1 else [arg for arg in args]
+
+    E = get_possible_pairs_leading_to_infinite_loop(banana_list)  # E: set of all possible pairs leading to an infinite loop
+    MCM = get_maximum_cardinality_matching(E)  # MCM: maximum cardinality matching
+
+    return len(banana_list) - 2 * MCM
+
 
 def get_maximum_cardinality_matching(S):
-    # (?) blossom algorithm
-    return 0
+    # (?) blossom algorithm (maximum cardinality matching (maximum independent edge set))
+    # (X) greedy algorithm
+    
+    if len(S) == 0:
+        return 0
+
+    M = set()  # M: matching
+
+    # Initialize a dictionary to represent the adjacency dict of G=(V,E)
+    adjacency_dict = dict()
+    for x, y in S:
+        if not x in adjacency_dict:
+            adjacency_dict[x] = set()
+        if not y in adjacency_dict:
+            adjacency_dict[y] = set()
+
+        adjacency_dict[x].add(y)
+        adjacency_dict[y].add(x)
+
+    # <bad code>
+    sorted_keys = sorted(adjacency_dict.keys(), key=lambda k: len(adjacency_dict[k]))
+
+    while len(sorted_keys) > 1:
+        sorted_keys = sorted(sorted_keys, key=lambda k: len(adjacency_dict[k]))
+        key = sorted_keys[0]
+
+        sorted_values = sorted(adjacency_dict[key], key=lambda value: len(adjacency_dict[value]))
+
+        if len(sorted_values) > 0:
+            chosen_value = sorted_values[0]
+
+            M.add((key, chosen_value))
+            adjacency_dict.pop(key)
+            adjacency_dict.pop(chosen_value)
+            for k in adjacency_dict:
+                adjacency_dict[k].discard(chosen_value)
+                adjacency_dict[k].discard(key)
+            sorted_keys.remove(chosen_value)
+
+        sorted_keys.remove(key)
+    # </bad code>
+
+    return len(M)
+
 
 def get_possible_pairs_leading_to_infinite_loop(L):
-    S = set()
+    # (use indices, to handle the case of multiple trainers with the same number of bananas)
+    P = [(ida, idb + ida + 1) for ida, a in enumerate(L) for idb, b in enumerate(L[ida + 1 :]) if lead_to_infinite_loop(a, b)]
+    return P
 
-    for i in L:
-        for j in L:
-            # ? i and j same item case
-            if lead_to_infinite_loop(i, j):
-                if j > i:
-                    S.add((i, j))
-                else:
-                    S.add((j, i))
 
-    
-    return S
-
-def lead_to_infinite_loop(x, y) -> bool:
+def lead_to_infinite_loop(x, y):
     """
-    Checks if x and y lead to an infinite loop, ie iff   with y >= x    y / x != 2 ^ (n + 1) - 1    ∀ n ∈ N ∪ {0}
-    
+    Checks if x and y lead to an infinite loop, ie iff   with y >= x    y / x != 2 ^ (n + 1) - 1    for any n belong to N U {0}
+
     Parameters:
         x (int): Element 1.
         y (int): Element 2.
@@ -33,25 +70,26 @@ def lead_to_infinite_loop(x, y) -> bool:
     Returns:
         bool: True if it leads to an infinite loop, False otherwise.
     """
-    
+
     # Ensure that y >= x
     if x > y:
         x, y = y, x
 
-    # If the remainder of y / x division is not zero, the equation cannot be 
+    # If the remainder of y / x division is not zero, the equation cannot be
     # satisfied for any value of n (x and y lead to an infinte loop)
     r = y % x
     if r != 0:
         return True
-    
+
     # If (y / x) + 1 is a power of 2, the equation can be satisfied for a value
     # of n (x and y do not lead to an infinte loop)
-    q = y//x
+    q = y // x
     q += 1
-    if q & (q-1) == 0: # Check if q is a power of 2 (using bit manipulations) 
+    if q & (q - 1) == 0:  # Check if q is a power of 2 (using bit manipulations)
         return False
     else:
         return True
+
 
 def main():
     assert lead_to_infinite_loop(7, 7) == False
@@ -64,7 +102,13 @@ def main():
     assert lead_to_infinite_loop(5, 15) == False
     assert lead_to_infinite_loop(5, 35) == False
 
-    # solution([1, 7, 3, 21, 13, 19])
+    assert solution([1, 7, 3, 21, 13, 19]) == 0
+    assert solution([1, 1]) == 2
+    assert solution(1, 1) == 2
+    assert solution([3, 3, 2, 6, 6]) == 1
+    assert solution([1, 7, 21]) == 1
+    assert solution(1, 2) == 0
+
 
 if __name__ == "__main__":
     main()
